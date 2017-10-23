860053d608827a7a9318179daa22cddd12ebcf2a
MasonHolland <masontholland@gmail.com>
2017-10-23 09:46:01 -0600

    figuring out selectors to  append to top word

--- wordEvents.js
+++ wordEvents.js
@@ -3,9 +3,12 @@ var api = 'https://wordwatch-api.herokuapp.com'
 
 function retrieveTopWord() {
   $.get( api + '/api/v1/top_word', function( data ) {
-    console.log(data)
-    // $('.top-word').append( data );
-    // console.log( data + 'successfully appended to top word' );
+    debugger;
+    var key   = Object.keys(data.word[0]);
+    var value = Object.values(data.word[0]);
+    $('.top-word > h3')
+    .append(
+      '<span>' + key + '(' + value +')' + '</span>' );
   });
 }
 
