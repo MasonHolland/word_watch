7ce98fe45c7fbdcc5cefe4bfb5ff6dc3dceb8c21
MasonHolland <masontholland@gmail.com>
2017-10-23 11:05:57 -0600

    word count logic is working

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 var wordEvents = require('./wordEvents')
+var $          = require('jquery')
 
 document.addEventListener("DOMContentLoaded", () => {
   wordEvents.retrieveAndAppendTopWord()
+
+  $(':button').click(function() {
+    var textBlock = $('textarea').val();
+    wordEvents.breakDown(textBlock);
+  })
 })
--- src/wordEvents.js
+++ src/wordEvents.js
@@ -3,7 +3,7 @@ var Ajax    = require('./ajaxRequests')
 var api = 'https://wordwatch-api.herokuapp.com'
 
 
-class HtmlHelper {
+class WatchEvents {
   static retrieveAndAppendTopWord() {
     $.get( api + '/api/v1/top_word', function( data ) {
       var topWord       = Object.keys(data.word)[0]
@@ -11,6 +11,21 @@ class HtmlHelper {
       $('.top-word > h3').append(topWord + ' (' + topWordCount +')');
    })
   }
+
+  static breakDown(data) {
+    var noPunctuation = data.replace(/[^\w\s]|_/g, "").replace(/\s+/g, " ");
+    var allWords = noPunctuation.split(" ");
+    var wordCounts = {};
+    allWords.forEach(function(word) {
+      var filtered = word.toLowerCase();
+      if (wordCounts[filtered] !== undefined) {
+        wordCounts[filtered]++;
+      } else {
+        wordCounts[filtered] = 1;
+      }
+    })
+    debugger;
+  }
 }
 
-module.exports = HtmlHelper
+module.exports = WatchEvents
